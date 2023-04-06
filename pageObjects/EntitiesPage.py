680abaa45f41a76788564b10b8df644d3c778288
KamranAutomation <kamran1300@protonmail.com>
2023-04-07 01:08:49 +0500

    Allhumdullia Full Customer and Invoice integration is passed with Pass and failed status check

--- pageObjects/EntitiesPage.py
+++ pageObjects/EntitiesPage.py
@@ -1,4 +1,5 @@
 import time
+import re
 from telnetlib import EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -41,10 +42,25 @@ class EntitiesPage:
     Run_Your_Autymation_button_xpath = "//div[@fxlayoutalign='center center']//button[@role='button']"
     Intuit_popup_email_selecting_button_xpath = "//span[normalize-space()='Email a code']"
     Intuit_popup_verification_code_continue_button_xpath = "//span[normalize-space()='Continue']"
-    Review_validation_of_Enitity_Success = "//h4[normalize-space()='Success: 2']"
-    Review_validation_of_Enitity_All = "//span[normalize-space()='All: 2']"
-    Review_validation_of_Enitity_Failed = "//h4[normalize-space()='Failed: 0']"
-    Review_validation_of_Enitity_page_button_Finish = "//button[normalize-space()='Finish']"
+    Review_validation_of_Enitity_Success_xpath = "//h4[normalize-space()='Success: 2']"
+    Review_validation_of_Enitity_All_xpath = "//span[normalize-space()='All: 2']"
+    Review_validation_of_Enitity_Failed_xpath = "//h4[normalize-space()='Failed: 0']"
+    Review_validation_of_Enitity_page_button_Finish_xpath = "//button[normalize-space()='Finish']"
+    select_source_Invoice_entity_destination_item_xpath = "//span[contains(text(),'Invoice')]"
+    select_source_file_connection_entity_xpath = "(//span[@title='Autymate_Sample_Source_Data_File'][normalize-space()='Autymate_Sample_Source_ ...'])[1]"
+    step1_entity_text_field_cssselector = "input[placeholder='Search...']"
+    Invoice_excel_sheet_tab_selection_xpath = "//span[@title='Invoices']"
+    Qbo_connection_Tab_selection_xpath = "(//span[@title='QuickBooks Online'][normalize-space()='QuickBooks Online'])[1]"
+    Qbo_settings_Income_Account_New_Products_Services_xpath = "(//span[@id='bfcf91b9-391f-49b8-a0a0-399edab96413'])[1]"
+    Qbo_settings_Expense_Account_New_Products_Services_xpath = "//div[@fxlayout='column']/div[@fxlayout='column']/div/div[@fxlayout='column']/div/app-auty-workflow-step/div[@fxlayout='column']/div[@fxlayout='row wrap']/div/div[@fxlayout='column']/div/app-auty-widget/div[@fxlayout='column']/div/form[@role='form']/formly-form/formly-field/app-auty-general-hub-query-settings-formly/app-auty-general-hub-query-settings/div[@fxflexlayout='row']/div[@fxflexlayout='row']/kendo-tabstrip/div[@role='tabpanel']/div/div/div[@fxlayout='row wrap']/div[1]/div[1]/kendo-dropdownlist[1]/span[@class='k-input']"
+    Qbo_setting_discount_dropdown_option_cssselector = "#f5b6580c-4ddf-4b3e-ba2f-723993f10f01"
+    Qbo_setting_advertising_dropdown_option_xpath = "//span[contains(text(),'Advertising')]"
+    # Review_validation_of_Enitity_Counts_xpath ="//li[contains(@class,'k-state-default') and contains(@class,'k-state-active')]/span[contains(@class,'k-link') and contains(text(),'All')]"
+    Invoice_Review_validation_of_Enitity_All_xpath = "//span[normalize-space()='All: 3']"
+    Invoice_Review_validation_of_Enitity_Success_xpath = "//h4[normalize-space()='Success: 3']"
+    Invoice_Review_validation_of_Enitity_Failed_xpath = "//h4[normalize-space()='Failed: 0']"
+
+
 
     def __init__(self, driver):
         self.driver = driver
@@ -174,21 +190,21 @@ class EntitiesPage:
 
         global all_count, success_count, failed_count
         try:
-            all_count_element = self.driver.find_element(By.XPATH, self.Review_validation_of_Enitity_All)
+            all_count_element = self.driver.find_element(By.XPATH, self.Review_validation_of_Enitity_All_xpath)
             all_count_text = all_count_element.text
             all_count = int(all_count_text.split()[-1])
         except NoSuchElementException:
             print("All count not found")
 
         try:
-            success_count_element = self.driver.find_element(By.XPATH, self.Review_validation_of_Enitity_Success)
+            success_count_element = self.driver.find_element(By.XPATH, self.Review_validation_of_Enitity_Success_xpath)
             success_count_text = success_count_element.text
             success_count = int(success_count_text.split()[-1])
         except NoSuchElementException:
             print("Success count not found")
 
         try:
-            failed_count_element = self.driver.find_element(By.XPATH, self.Review_validation_of_Enitity_Failed)
+            failed_count_element = self.driver.find_element(By.XPATH, self.Review_validation_of_Enitity_Failed_xpath)
             failed_count_text = failed_count_element.text
             failed_count = int(failed_count_text.split()[-1])
         except NoSuchElementException:
@@ -199,5 +215,85 @@ class EntitiesPage:
             print("Test case passed")
         else:
             print("Test case failed")
+
+                    ################################### Invoice ##############################################
+
+    def Invoice_entity_selection(self):
+        # /////////////////////////Choose destination to see relevant options below////////////////////////////
+        self.driver.find_element(By.XPATH, self.select_source_entity_destination_dropdown_xpath).click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, self.select_source_Invoice_entity_destination_item_xpath).click()
+        time.sleep(10)
+        # /////////////////////////////////////Work Flow Button///////////////////////////////////////////////
+        self.driver.find_element(By.XPATH, self.select_use_workflow_button_xpath).click()
+
+    def select_Source_file(self):
+
+        self.driver.find_element(By.XPATH, self.select_source_file_connection_entity_xpath).click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.next_step_button_in_flow_xpath).click()
+
+    def Invoice_excel_sheet_Tab(self):
+
+        self.driver.find_element(By.CSS_SELECTOR, self.step1_entity_text_field_cssselector).click()
+        self.driver.find_element(By.CSS_SELECTOR, self.step1_entity_text_field_cssselector).send_keys("Invoice")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.Invoice_excel_sheet_tab_selection_xpath).click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.next_step_button_in_flow_xpath).click()
+
+    def QBO_connection_Tab(self):
+
+        self.driver.find_element(By.XPATH, self.Qbo_connection_Tab_selection_xpath).click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.next_step_button_in_flow_xpath).click()
+        time.sleep(10)
+        # self.driver.find_element(By.XPATH, self.Qbo_settings_Income_Account_New_Products_Services_xpath).click()
+        # time.sleep(5)
+        # self.driver.find_element(By.CSS_SELECTOR, self.Qbo_setting_discount_dropdown_option_cssselector).click()
+        # time.sleep(5)
+        # self.driver.find_element(By.XPATH, self.Qbo_settings_Expense_Account_New_Products_Services_xpath).click()
+        # time.sleep(5)
+        # self.driver.find_element(By.XPATH, self.Qbo_setting_advertising_dropdown_option_xpath).click()
+
+
+    def Invoice_entity_validation_status_check(self):
+
+        global all_count, success_count, failed_count
+        try:
+            all_count_element = self.driver.find_element(By.XPATH, self.Invoice_Review_validation_of_Enitity_All_xpath)
+            all_count_text = all_count_element.text
+            all_count = int(all_count_text.split()[-1])
+        except NoSuchElementException:
+            print("All count not found")
+
+        try:
+            success_count_element = self.driver.find_element(By.XPATH, self.Invoice_Review_validation_of_Enitity_Success_xpath)
+            success_count_text = success_count_element.text
+            success_count = int(success_count_text.split()[-1])
+        except NoSuchElementException:
+            print("Success count not found")
+
+        try:
+            failed_count_element = self.driver.find_element(By.XPATH, self.Invoice_Review_validation_of_Enitity_Failed_xpath)
+            failed_count_text = failed_count_element.text
+            failed_count = int(failed_count_text.split()[-1])
+        except NoSuchElementException:
+            print("Failed count not found")
+
+        # Compare the counts to the expected values and print the result
+        if all_count == success_count and failed_count == 0:
+            print("Test case passed")
+        else:
+            print("Test case failed")
+
+
+
+
+
+
+
+
+
 
 
